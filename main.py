7969a12db71f137323ee6543629a38d651473bd0
felipe-clark <felipe.clark@gmail.com>
2019-11-24 18:33:40 -0500

    Provide alpha version of the application

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import argparse
 import os
 import vignette_correction
+import cv2
+import numpy as np
+import rawpy
 
 if __name__ == "__main__":
     # ----- Command line argument parser -----
@@ -23,7 +26,13 @@ if __name__ == "__main__":
     lens_model = args['lens_model']
     noise_red_mth = args['noise_red_mth']
 
-    # Validating command line options
+    # Validating and formatting command line options
+    if not in_path.endswith("/"):
+        in_path += "/"
+
+    if not out_path.endswith("/"):
+        out_path += "/"
+
     if vig_red_mth == 'lens' and lens_model == None:
         print("Lens model image file is required for the chosen vignette removal method.\n" +
               "Please provide a lens model image using the -lens_model parameter. Use -help for further assistance.\nAborting...")
@@ -31,24 +40,39 @@ if __name__ == "__main__":
 
     vig_aux_data = {'lens_model': lens_model, 'out_path': out_path, 'vignette_tool_path': vignette_tool_path, 'matlab_path': matlab_path}
 
+    # Auxiliary variagles
+    kernel = np.ones((5, 5), np.float32) / 25
+    if vig_red_mth == 'lens':
+        std_vig_model = vignette_correction.load_vig_model(lens_model)
 
     for filename in os.listdir(in_path):
-        if filename.endswith(".cr2"): 
+        if filename.endswith(".CR2"): 
+            with rawpy.imread(in_path + filename) as raw:
+                image = raw.postprocess(use_camera_wb=True, output_color=rawpy.ColorSpace.sRGB, no_auto_bright=True)
+
             # ----- Noise Reduction-----
             if noise_red_mth == 'low-pass':
                 print("Low pass")
+                image = cv2.GaussianBlur(cv2.cvtColor(image, cv2.COLOR_RGB2BGR), (5,5), 0)
             elif noise_red_mth == 'average':
                 print("Average")
+                image = cv2.filter2D(image,-1,kernel)
             elif noise_red_mth == 'median':
                 print("Median")
+                image = cv2.medianBlur(image,5)
             else:
                 print("No noise filtering method will run.")
 
+            brg_out = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            cv2.imwrite(out_path + "denoise_only/" + filename.split('.')[0] + ".tif", brg_out)
+
 
             # ----- Lens Model Vignette Removal -----
-            image = None # TODO: image must be updated to the opened image matrix
             if vig_red_mth == 'lens':
-                vignette_correction.run_method(vig_red_mth, vig_aux_data, image)
+                out_img = vignette_correction.run_method(vig_red_mth, vig_aux_data, image, std_vig_model)
+
+                brg_out = cv2.cvtColor(out_img, cv2.COLOR_RGB2BGR)
+                cv2.imwrite(out_path + filename.split('.')[0] + ".tif", brg_out)
 
 
     if vig_red_mth == 'auto':
--- vignette_correction.py
+++ vignette_correction.py
@@ -1,6 +1,56 @@
 import os
+import numpy as np
+from PIL import Image
+import cv2
+import rawpy
 
-def run_method(vig_red_mth, vig_aux_data, image=None):
+def correct_channel(img_array, channel, std_vig_model):
+  img_channel = img_array[:, :, channel]
+  
+  #Standardise array.
+  img_max = np.amax(img_channel) 
+  std_img = img_channel / img_max
+
+  #Divide the two arrays to remove vignetting.
+  corrected_img = std_img / std_vig_model
+
+  #Standardise corrected array.
+  corrected_max = np.amax(corrected_img) 
+  std_corrected = corrected_img / corrected_max
+
+  #Reconstruct the output image.
+  output_img_channel = std_corrected * img_max  
+
+  return output_img_channel
+
+def remove_vig(img_array, std_vig_model):
+  r = correct_channel(img_array, 0, std_vig_model)
+  g = correct_channel(img_array, 1, std_vig_model)
+  b = correct_channel(img_array, 2, std_vig_model)
+
+  rgbArray = np.zeros((r.shape[0], r.shape[1], 3), 'uint8')
+  rgbArray[..., 0] = r
+  rgbArray[..., 1] = g
+  rgbArray[..., 2] = b
+
+  return rgbArray
+    
+
+def load_vig_model(model):
+  #Import a grayscale test image of a uniform background.
+  with rawpy.imread(model) as raw:
+    rgb = raw.postprocess(use_camera_wb=True, output_color=rawpy.ColorSpace.sRGB, no_auto_bright=True)
+
+  vig_model_grey = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)
+
+  #Standardise grayscale array by setting max pixel value to 1.
+  max_vig_model = np.amax(vig_model_grey) 
+  std_vig_model = vig_model_grey / max_vig_model
+
+  return std_vig_model
+
+
+def run_method(vig_red_mth, vig_aux_data, image=None, std_vig_model=None):
     '''
     Performs vignette correction.
     vig_aux_data is a dictionary containing:
@@ -12,12 +62,11 @@ def run_method(vig_red_mth, vig_aux_data, image=None):
     image: image whose vignette needs to be fixed. Can be set to None if method is auto.
     '''
 
-    lens_model = vig_aux_data.get('lens_model')
     out_path = vig_aux_data.get('out_path')
     vignette_tool_path = vig_aux_data.get('vignette_tool_path')
     matlab_path = vig_aux_data.get('matlab_path')
 
-    print(vig_red_mth, lens_model, out_path, vignette_tool_path, matlab_path)
+    print(vig_red_mth, out_path, vignette_tool_path, matlab_path)
 
     if vig_red_mth == 'auto':
             # Set up the Matlab vignette removal tool
@@ -28,4 +77,4 @@ def run_method(vig_red_mth, vig_aux_data, image=None):
             os.system(vignette_fix_run_command)
 
     elif vig_red_mth == 'lens':
-        print("Using lens model for vignette removal")
+        return remove_vig(image, std_vig_model)
